452298518f6432ddefe0ac15c80defbd093f4b2b
Dnyaneshwar-Padule <dnyaneshwarpadule83@gmail.com>
2025-12-15 15:36:13 +0530

    Multithreading: Intro-To-Multithreading

--- Socket-Programming/03-File-Transfer/src/com/tca/Server.java
+++ Socket-Programming/03-File-Transfer/src/com/tca/Server.java
@@ -2,6 +2,7 @@ package com.tca;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -29,14 +30,22 @@ public class Server {
 			String fileName = inputStream.readUTF();
 			System.out.println("Requested file : " + fileName);
 			
+			long size = 0;
+			long bytesSent = 0;
+			
 			try {
 				
 				FileInputStream fis = new FileInputStream(fileName);
-				byte buffer[] =  new byte[8192];
+				File file = new File(fileName);
+				size = file.length();
+				
+				byte buffer[] =  new byte[8192 * 1024];
 				int bytesRead;
 				
 				while( (bytesRead = fis.read(buffer)) != -1 ) {
+					bytesSent += bytesRead;
 					outputStream.write(buffer, 0, bytesRead);
+					System.out.println( (((double)bytesSent/(double)size)*100) + "%" );
 				}
 				
 				// Send remaining bytes...
